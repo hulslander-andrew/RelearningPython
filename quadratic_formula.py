4f232462487d1d72b71d16c973a50c6a19f976dd
Andrew Hulslander <54474171+hulslander-andrew@users.noreply.github.com>
2020-12-24 01:06:10 -0500

    Update quadratic_formula.py

--- quadratic_formula.py
+++ quadratic_formula.py
@@ -13,3 +13,5 @@ root_2 = (B * -1 - ((B ** 2) - 4 * A * C) ** 0.5) / (2 * A)
 
 print("x = " + str(root_1)) 
 print("x = " + str(root_2))
+
+#I do not know how to include complex numbers yet so imaginary numbers cause an error
